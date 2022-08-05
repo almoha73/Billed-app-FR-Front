2c483da841b822bd16625dde40ac9ab0855594e1
Agnes <almoha@gmail.com>
2022-08-05 09:21:03 +0200

    correction html loginUI.js

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -17,10 +17,10 @@ export default () => {
             <div class="card">
               <div class="card-body">
                 <form class="form-signin" data-testid="form-employee">
-                  <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
-                  <label for="inputEmail">Votre email</label>
+                  <h2 class="h3 mb-3 font-weight-normal">Employé</h2>
+                  <label for="email">Votre email</label>
                   <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
+                  <label for="password">Mot de passe</label>
                   <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
@@ -31,10 +31,10 @@ export default () => {
             <div class="card">
               <div class="card-body">
                 <form class="form-signin" data-testid="form-admin">
-                  <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
-                  <label for="inputEmail">Votre email</label>
+                  <h2 class="h3 mb-3 font-weight-normal">Administration</h2>
+                  <label for="email">Votre email</label>
                   <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
+                  <label for="password">Mot de passe</label>
                   <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>

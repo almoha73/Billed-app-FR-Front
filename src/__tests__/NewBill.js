fcc38a9cf94604282f7e5169d4429db072d2c162
Agnes <almoha@gmail.com>
2022-09-01 16:03:17 +0200

    rangement et dry des tests

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -31,10 +31,9 @@ describe("When I am on NewBill Page", () => {
       root.setAttribute("id", "root")
       document.body.appendChild(root)
       router()
+      window.onNavigate(ROUTES_PATH["NewBill"]);
   });
   test("Then mail icon in vertical layout should be highlighted ", async () => {
-    
-    window.onNavigate(ROUTES_PATH["NewBill"]);
     await waitFor(() => screen.getByTestId("icon-mail"));
     const windowIcon = screen.getByTestId("icon-mail");
     expect(windowIcon.classList.contains("active-icon")).toBe(true);
@@ -42,19 +41,82 @@ describe("When I am on NewBill Page", () => {
   });
 
   test("Then it should show the new bill form", () => {
-    window.onNavigate(ROUTES_PATH["NewBill"]);
-    // const html = NewBillUI();
-    // document.body.innerHTML = html;
-
     expect(screen.getByTestId("form-new-bill")).toBeTruthy();
   });
 
-  describe("when i click on the submit button", () => {
-    test("the handleSubmit function is called", () => {
-      window.onNavigate(ROUTES_PATH["NewBill"]);
-      // const html = NewBillUI();
-      // document.body.innerHTML = html;
+  describe("When I select a file and the file format is valid", () => {
+    test('it should update the input field', () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      const newBillObject = new NewBill({
+       document,
+       onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
+       store: mockStore,
+       localStorage: window.localStorage
+      })
+       //window.onNavigate(ROUTES_PATH["NewBill"]);
+     const handle = jest.fn((e) => newBillObject.handleChangeFile(e))
+     const inputFile = screen.getByTestId('file')
+     const img = new File(['img'], 'image.png', {type:'image/png'})
+     
+     inputFile.addEventListener('change', (e) => {
+         handle(e)
+     })
+     fireEvent.change(inputFile, {
+      target: {
+        files: [img],
+      },
+    })
+    // //userEvent.upload(inputFile, img) fonctionne aussi
+     
+    expect(handle).toHaveBeenCalled()
+    expect(inputFile.files[0]).toStrictEqual(img)
+       expect(inputFile.files[0].name).toBe('image.png')
+   
+    
+    })
+
+    
+  });
+
+  describe("When I select a file and the file format is not valid", () => {
+    test('it should not update the input field',  () => {
       
+       const html = NewBillUI()
+       document.body.innerHTML = html
+       const newBillObject = new NewBill({
+        document,
+        onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
+        store: mockStore,
+        localStorage: window.localStorage
+       })
+       window.alert = jest.fn();
+       
+      const handle = jest.fn((e) => newBillObject.handleChangeFile(e))
+      const inputFile = screen.getByTestId('file')
+      
+      inputFile.addEventListener('change', (e) => {
+        handle(e)
+      })
+      const file = new File(['img'], 'test.pdf', {type:'application/pdf'})
+      userEvent.upload(inputFile, file)
+      expect(handle).toHaveBeenCalled()
+      
+     expect(inputFile.files[0]).toStrictEqual(file)
+      expect(inputFile.files[0].name).toBe('test.pdf')
+      expect(window.alert).toBeCalled()
+      
+      
+      
+    })
+  
+  })
+
+  describe("when i click on the submit button", () => {
+    
+    test("the handleSubmit function is called", () => {
+      const html = NewBillUI()
+       document.body.innerHTML = html
       const onNavigate = pathname => {
         const html = ROUTES({ pathname, data: [] });
         document.body.innerHTML = html;
@@ -75,7 +137,6 @@ describe("When I am on NewBill Page", () => {
     });
 
     test("the update bills function is executed with informations provided && send to the mock API POST", async () => {
-      window.onNavigate(ROUTES_PATH["NewBill"]);
       
       const onNavigate = jest.fn()
       const store = null;
@@ -124,10 +185,9 @@ describe("When I am on NewBill Page", () => {
         
     });
 
-    
+
     test("Then it should send the new bill to the mock API POST and fails with 404 message error", async () => {
-      
-      window.onNavigate(ROUTES_PATH["NewBill"]);
+     
       mockStore.bills.mockImplementationOnce(() => {
         return {
           create: () =>{
@@ -138,24 +198,20 @@ describe("When I am on NewBill Page", () => {
           }
         }})
         
-       
-      const store = mockStore;
-
-      const newBillObject = new NewBill({
-        document,
-        onNavigate,
-        store,
-        localStorage,
-      });
-      
-       const formNewBill = screen.getByTestId("form-new-bill")
-       const handleSubmit = jest.fn((e) => newBillObject.handleSubmit(e))
-       formNewBill.addEventListener("submit", handleSubmit)
-       fireEvent.submit(formNewBill)
-       expect(handleSubmit).toHaveBeenCalled()
-       await new Promise(process.nextTick)
-       
-      })
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+       const html = BillsUI({ error: "Erreur 404" });
+       document.body.innerHTML = html;
+       const message = screen.getByText(/Erreur 404/);
+       expect(message).toBeTruthy();
+       console.error = jest.fn()
+       expect.assertions(1);
+       try{
+         mockStore.bills
+       }catch{
+         expect (error).toEqual(console.error)
+       }
+     });
 
     test("Then it should send the new bill to the mock API POST and fails with 500 message error", async () => {
       mockStore.bills.mockImplementationOnce(() => {
@@ -173,86 +229,27 @@ describe("When I am on NewBill Page", () => {
       }
     });
       window.onNavigate(ROUTES_PATH.Bills)
-       //await new Promise(process.nextTick);
+       await new Promise(process.nextTick);
       const html = BillsUI({ error: "Erreur 500" });
       document.body.innerHTML = html;
       const message = screen.getByText(/Erreur 500/);
       expect(message).toBeTruthy();
-      
+      console.error = jest.fn()
+       expect.assertions(1);
+       try{
+          mockStore.bills
+       }catch{
+         expect (error).toEqual(console.error)
+       }
     });
+
+   
           
   });
 })
 
 
-  describe("When I select a file and the file format is valid", () => {
-      test('it should update the input field', () => {
-        
-        const html = NewBillUI()
-        document.body.innerHTML = html
-        const newBillObject = new NewBill({
-         document,
-         onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
-         store: mockStore,
-         localStorage: window.localStorage
-        })
-         window.onNavigate(ROUTES_PATH["NewBill"]);
-       const handle = jest.fn((e) => newBillObject.handleChangeFile(e))
-       const inputFile = screen.getByTestId('file')
-       const img = new File(['img'], 'image.png', {type:'image/png'})
-       
-       inputFile.addEventListener('change', (e) => {
-           handle(e)
-       })
-       fireEvent.change(inputFile, {
-        target: {
-          files: [img],
-        },
-      })
-      //userEvent.upload(inputFile, img) fonctionne aussi
-       
-       expect(handle).toHaveBeenCalled()
-       expect(inputFile.files[0]).toStrictEqual(img)
-        expect(inputFile.files[0].name).toBe('image.png')
-     
-      
-      })
-
-      
-})
-
-describe("When I select a file and the file format is not valid", () => {
-      test('it should not update the input field',  () => {
-        
-         const html = NewBillUI()
-         document.body.innerHTML = html
-         const newBillObject = new NewBill({
-          document,
-          onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
-          store: mockStore,
-          localStorage: window.localStorage
-         })
-         window.alert = jest.fn();
-         
-        const handle = jest.fn((e) => newBillObject.handleChangeFile(e))
-        const inputFile = screen.getByTestId('file')
-        
-        inputFile.addEventListener('change', (e) => {
-          handle(e)
-        })
-        const file = new File(['img'], 'test.pdf', {type:'application/pdf'})
-        userEvent.upload(inputFile, file)
-        expect(handle).toHaveBeenCalled()
-        
-       expect(inputFile.files[0]).toStrictEqual(file)
-        expect(inputFile.files[0].name).toBe('test.pdf')
-        expect(window.alert).toBeCalled()
-        
-        
-        
-      })
-    
-  })
+  
 
 
 
